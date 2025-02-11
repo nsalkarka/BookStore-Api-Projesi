ddb5fb3ee64be1304aeb2dde8a702c6405f59075
nsalkarka <_karka67@windowslive.com>
2025-02-11 21:59:55 +0300

    update

---
+++ BookStore.DataAccessLayer/Context/BookStoreContext.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccessLayer.Context
+{
+    public class BookStoreContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-LDFLC27\\SQLEXPRESS;initial catalog=ApiBookDb;integrated security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+//trustservercertificate=true;
+//Plurulize
---
+++ BookStore.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using BookStore.DataAccessLayer.Abstract;
+using BookStore.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly BookStoreContext _context;
+
+        public GenericRepository(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        //BookStoreContext c=new BookStoreContext();  bağımlı bu yukarıdaki field ile dependency injection yapılıyor generic repository oluştuğu anda bu sınıftan bir nesne örneği türetiyor
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var value = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(value);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
